89c3f61554ebef38a0094e8424d5f63022d93f00
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-09-11 08:40:50 +0300

    Adding objservables module

--- directives/src/main.ts
+++ directives/src/main.ts
@@ -9,5 +9,21 @@ bootstrapApplication(AppComponent).catch((err) => console.error(err));
 
 // Directives are an enhancement to the component
 // ex:- (ngModle) => that add extra functionality to the input field by making it's value be awaird of in the components file (.ts file)
+
+// (ngModle) is not only for 2 way binding,
+// but it also adds dynamic css classes for the field like (ng-untouched - ng-touched) and that is useful for adding dynamic styling for the field also
+// (ng) in the start referce that that thing made be angular team.
+
 // (ngModule) => is a (built-in diractive)
 // We can also (custom directives) for our project
+
+// (ngModule) and any directive like it called (((attribute directives)))
+// (*ngIf) and any directive like it called (((structural directives)))
+
+// Directives types => 1) Attribute directives - 2) Structural directives - #) Custom directives
+
+// _________________________________________________________
+// _________________________________________________________
+
+// [Computed()] usage => 1) like gitter function but for signals - 2) compute a new value from a signal
+// ex (on 2):- isAdmin = computed(() => this.authService.activePermission() === 'admin')
