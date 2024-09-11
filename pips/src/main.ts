3bd7a0ad9bf3f8fff39f3c757f27171446dbe951
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-09-11 09:01:31 +0300

    Adding pips module

--- pips/src/main.ts
+++ pips/src/main.ts
@@ -4,6 +4,3 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent)
   .catch((err) => console.error(err));
-
-  // (Objservables) are not intreduced in angular, but intreduces in (RXJS) library.
-  // (Objservable) is an object that [produces and use] (((a stream of data))).
