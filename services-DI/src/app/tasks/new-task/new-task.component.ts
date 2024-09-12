cd3ba68758079c8948967f54f51168b78cdc1a76
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-09-12 11:42:16 +0300

    making tasks service and inject it for new tasks and tasks list

--- services-DI/src/app/tasks/new-task/new-task.component.ts
+++ services-DI/src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-new-task',
@@ -11,7 +12,11 @@ import { FormsModule } from '@angular/forms';
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
 
+  constructor(private addTaskData: TaskService) {}
+
   onAddTask(title: string, description: string) {
     this.formEl()?.nativeElement.reset();
+
+    this.addTaskData.addTask({ title, description });
   }
 }
