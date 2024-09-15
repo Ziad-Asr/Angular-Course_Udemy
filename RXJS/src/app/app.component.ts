934898e3d6d69cfd0cdda2cd2cd010facc4b5b78
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-09-15 16:24:38 +0300

    finish observables module

--- RXJS/src/app/app.component.ts
+++ RXJS/src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  effect,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -6,20 +14,67 @@ import { interval, map } from 'rxjs';
   standalone: true,
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
-  private destroyRef = inject(DestroyRef);
+export class AppComponent {
+  // ##################
+  // ### Observable ###
+  // ##################
+  // private destroyRef = inject(DestroyRef);
+  // ngOnInit(): void {
+  //   const subscription = interval(1000)
+  //     .pipe(map((val) => val * 2))
+  //     .subscribe({
+  //       next: (val) => console.log(val),
+  //       complete: () => console.log('Completed'),
+  //       error: () => console.log('Error'),
+  //     });
+  //   this.destroyRef.onDestroy(() => {
+  //     subscription.unsubscribe();
+  //   });
+  // }
+  // -------------------------------------------------------------------------
+  // -------------------------------------------------------------------------
+  // ##############
+  // ### Signal ###
+  // ##############
+  // clickCount = signal(0);
+  // constructor() {
+  //   effect(() => {
+  //     console.log(`Clicked button ${this.clickCount()} times.`);
+  //   }); // This effect will always reexcute when ever a signal inside it changes
+  // }
+  // onClick() {
+  //   this.clickCount.update((prevValue) => prevValue + 1);
+  // }
+  // -------------------------------------------------------------------------
+  // -------------------------------------------------------------------------
+  // ############################
+  // ### Signal => Observable ###
+  // ############################
+  // clickCount = signal(0);
+  // // 1st way
+  // clickCount$ = toObservable(this.clickCount);
+  // // 2nd way
+  // constructor() {
+  //   toObservable(this.clickCount);
+  // }
+  // private destroyRef = inject(DestroyRef);
+  // ngOnInit(): void {
+  //   const subscription = this.clickCount$.subscribe({
+  //     next: (val) => console.log(`Clicked button ${this.clickCount()} times.`),
+  //   });
+  //   this.destroyRef.onDestroy(() => {
+  //     subscription.unsubscribe();
+  //   });
+  // }
+  // -------------------------------------------------------------------------
+  // -------------------------------------------------------------------------
+  // ############################
+  // ### Observable => Signal ###
+  // ############################
+  interval$ = interval(1000);
 
-  ngOnInit(): void {
-    const subscription = interval(1000)
-      .pipe(map((val) => val * 2))
-      .subscribe({
-        next: (val) => console.log(val),
-        complete: () => console.log('Completed'),
-        error: () => console.log('Error'),
-      });
-
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
-  }
+  intervalSignal = toSignal(this.interval$, {
+    initialValue: 0,
+    manualCleanup: true,
+  });
 }
--- RXJS/src/main.ts
+++ RXJS/src/main.ts
@@ -61,3 +61,48 @@ bootstrapApplication(AppComponent).catch((err) => console.error(err));
 
 // The differance between (BehaviorSubject - interval) that the (BehaviorSubject) 1)produce and 2)emit the value inside it
 // (interval) only produce the value and don't emit it.
+
+//--------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+// ###############
+// ### Signals ###
+// ###############
+// 1) set() => To give it a new value
+// 2) update(prevValue => {}) => same as [set()] but it provides the latest value of this signal
+// 3) computed() => like (getter function) in normal variables.
+
+// {{computed()}} in signal used like (getter function) in normal variables.
+// (more efficient, because it only changes when it's signal changes, (not using zone.js))
+// EX:-
+// 1) computed => imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+// 2) Getter => get imagePath() {
+//   return 'assets/users/' + this.selectedUser().avatar;
+//  }
+
+// effect(() => {
+//   console.log(`Clicked button ${this.clickCount()} times.`);
+// });
+// effect() used with signals
+// This effect will always reexcute when ever a signal inside it changes
+
+//--------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+// Observables looks like signals in many cases
+// like define data and emitting it, that can be done with both of them
+
+// ************************************
+// observable => values over (time)
+// signal => values in a (container)
+// ************************************
+
+// *************************************************
+// observable => greate for (events - stream data)
+// signal => greate for (application state)
+// *************************************************
+
+// *************************************************
+// observable => has no (initila value) [except {{ subjects }}]
+// signal => always has initial value
+// *************************************************
