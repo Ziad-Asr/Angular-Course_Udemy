d763546531f699ac208d2e2e41c5ef30e1aedf2c
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-09-16 18:24:29 +0300

    use http for fetching places and user-places, outsource the douplicated logic to the placs service

--- HTTP/src/app/places/places.service.ts
+++ HTTP/src/app/places/places.service.ts
@@ -1,20 +1,67 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 
 import { Place } from './place.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
   private userPlaces = signal<Place[]>([]);
+  private httpClient = inject(HttpClient); // returns an observable
 
   loadedUserPlaces = this.userPlaces.asReadonly();
 
-  loadAvailablePlaces() {}
+  loadAvailablePlaces() {
+    return this.fetchPlaces(
+      'http://localhost:3000/places',
+      'Something went wrong fetchong the available places. please try again later.'
+    );
+  }
 
-  loadUserPlaces() {}
+  loadUserPlaces() {
+    return this.fetchPlaces(
+      'http://localhost:3000/user-places',
+      'Something went wrong fetchong your favorite places. please try again later.'
+    ).pipe(
+      tap({
+        next: (userPlaces) => this.userPlaces.set(userPlaces),
+      })
+    );
+  }
 
-  addPlaceToUserPlaces(place: Place) {}
+  addPlaceToUserPlaces(place: Place) {
+    const prevPlaces = this.userPlaces();
+
+    if (!prevPlaces.some((p) => p.id === place.id)) {
+      this.userPlaces.set([...prevPlaces, place]);
+    }
+
+    return this.httpClient
+      .put('http://localhost:3000/user-places', {
+        placeId: place.id,
+      })
+      .pipe(
+        catchError((error) => {
+          this.userPlaces.set(prevPlaces);
+          return throwError(
+            () => new Error('Faild to store new selected place.')
+          );
+        })
+      );
+  }
 
   removeUserPlace(place: Place) {}
+
+  private fetchPlaces(url: string, errorMessage: string) {
+    return this.httpClient.get<{ places: Place[] }>(url).pipe(
+      map((resData) => resData.places),
+      catchError((error) => {
+        console.log(error);
+
+        return throwError(() => new Error(errorMessage));
+      })
+    );
+  }
 }
--- HTTP/src/main.ts
+++ HTTP/src/main.ts
@@ -1,6 +1,31 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
 
+bootstrapApplication(AppComponent, { providers: [provideHttpClient()] }).catch(
+  (err) => console.error(err)
+);
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+// { providers: [provideHttpClient()] } => I must add this to the (bootstrapApplication) to avoid (NullInjectorError)
+
+// [[ this.httpClient.get('http://localhost:3000/places', {...}) ]]
+// 1) {observe: 'response'} => This retruns the whole response (in next(response) => console.log(response.body?. .....)) => EX:- [ this.httpClient.get('http://localhost:3000/places', {observe: 'response'}) ]]
+// 2) {observe: 'events'} => This makes the (next()) runs 2 time 1)when request is sent (return {type: 0}) - 2) when response is come => EX:- [ this.httpClient.get('http://localhost:3000/places', {observe: 'events'}) ]]
+
+// this.httpClient.get(...).pip(catchError((error, obs) => {...}))
+// [[ catchError((error, obs) => {...} ]] => this a function offered by RXJS, that 1)gives the occured error 2)the observable return by (get) 3)return a new observable
+// Usally I use this with observables to return a (transformed error) which could be catched be (error()). [used only to keeo the logic inside the error() lean]
+
+// There are 3 functions I can apply on observable (inside the [subscribe] method)
+// 1) next() {} => run after response not always has status 200, could be anyhting else.
+// 2) completed() {}
+// 3) error() {}
+
+// ((( Tap() ))) operator
+// Tap() is a RXJS oprator that can accept an object of the same function can be used inside (subscribe({})) => [next(), error(), completed(), ...]
+// tap({
+//   next: (userPlaces) => this.userPlaces.set(userPlaces),
+// })
+
+//***** RXJS operators (up till now) => [map() - catchError() - tap()]
