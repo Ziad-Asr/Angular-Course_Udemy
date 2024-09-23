5e87a74934c4489d9023cfd1637f0ffa6c3947bc
ShipBee Technologies <123997217+Ziad-Asr@users.noreply.github.com>
2024-09-23 17:02:31 +0300

    working on navigation with dynamic routes and all ways to extract that dynamic route

--- routing/src/app/users/user-tasks/user-tasks.component.ts
+++ routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  inject,
+  input,
+  OnInit,
+} from '@angular/core';
+
+import { UsersService } from '../users.service';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet, RouterLink],
 })
-export class UserTasksComponent {}
+export class UserTasksComponent implements OnInit {
+  userId = input.required<string>();
+  private usersService = inject(UsersService);
+
+  userName = computed(
+    () => this.usersService.users.find((u) => u.id === this.userId())?.name
+  );
+
+  // Older way to get dynamic route
+  private activatedRoute = inject(ActivatedRoute);
+  userName_oldWay = '';
+
+  private distroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscription = this.activatedRoute.paramMap.subscribe({
+      next: (paramMap) =>
+        (this.userName_oldWay =
+          this.usersService.users.find((u) => u.id === paramMap.get('userId'))
+            ?.name || ''),
+    });
+
+    this.distroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+}
--- routing/src/main.ts
+++ routing/src/main.ts
@@ -1,5 +1,44 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+
+// Take a look on 1)app.routes 2)app.config 3)bootstrapApplication(AppComponent, appConfig)  {{{appConfig}}}
+
+// Making active route has spetcial styles => <a routerLink="/tasks" routerLinkActive="selected">
+
+// Anything doesn't work => Make sure that you imported it in component
+// Ex:- this is not working ((( routerLink="/tasks" ))) in template file until I type ((( imports: [RouterLink] ))) in component file
+
+// Navigate using dynamic routes => {{{ [routerLink]="['/users', user().id]" }}}
+
+// <a routerLink="tasks/new">Add Task</a>
+// here I used the route (( tasks/new )) only and this will move to the route (( /users/u2/tasks/new )) since it is a child to (/users/u2) route,
+// so I can simply type the rest of the route only to go to this child route
+
+// -----------------------------------------------------------------
+
+// // Extracting the (id) from the url
+// 1) providers: [provideRouter(routes, withComponentInputBinding())] => add [[[ withComponentInputBinding() ]]] to configurations
+// 2) use ((( userID = input.required<string>(); ))) in thecomponent
+
+// ------
+
+// // (( Older way )) to get dynamic route
+// private activatedRoute = inject(ActivatedRoute);
+// userName_oldWay = '';
+
+// ngOnInit(): void {
+//   this.activatedRoute.paramMap.subscribe({
+//     next: (paramMap) =>
+//       (this.userName_oldWay =
+//         this.usersService.users.find((u) => u.id === paramMap.get('userId'))
+//           ?.name || ''),
+//   });
+// }
+
+// -----------------------------------------------------------------
